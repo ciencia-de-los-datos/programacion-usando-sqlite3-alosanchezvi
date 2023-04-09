9aaa15cfceff2390b164f7155ec55cf9f2422f12
alosanchezvi <59671162+alosanchezvi@users.noreply.github.com>
2023-04-08 19:34:46 -0500

    Update pregunta_08.sql

--- pregunta_08.sql
+++ pregunta_08.sql
@@ -40,8 +40,9 @@
 --  3  2019  550.998571
 --
 --  >>> Escriba su codigo a partir de este punto <<<
-select  year(c23) as YEAR, avg(c21)
+select  
+DATE_FORMAT(c23, '%Y')  as YEAR, avg(c21)
 from tbl2
-group by  year(c23) ;
+group by  DATE_FORMAT(c23, '%Y');
 
 --
